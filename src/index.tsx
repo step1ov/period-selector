3319a8a3b16c6c0d52c0a04506a333b321eeab73
antonstepanov <antvstepanov@gmail.com>
2023-04-02 22:02:25 +0300

    Initial

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,11 @@
-import PeriodSelector, { PeriodSelectorDefault } from './components'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-export default PeriodSelector
-export { PeriodSelectorDefault }
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
